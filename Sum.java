c31408fd1ae281d5986db153bd798fbc87683fdc
Bhavishi Bansal <bansalbhavishi2003@gmail.com>
2023-05-23 20:15:00 +0530

    second commit

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class main{
   public static void main(String [] args){
     int num1 = 1;
     int num2 = 5;
-    System.out.println(num1 + num2);
+    System.out.println("bhavishi");
    }
 }
